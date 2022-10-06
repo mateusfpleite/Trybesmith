d2bca5f5821ca5de0669b22ac2bd98f020588adc
Mateus <mateusfpleite@gmail.com>
2022-10-06 17:47:37 -0300

    creates endpoint POST '/products'

--- src/Routes/products.routes.ts
+++ src/Routes/products.routes.ts
@@ -6,5 +6,5 @@ const productRouter = Router();
 const productController = new ProductController();
 
 productRouter.get('/products', productController.getAll);
-
+productRouter.post('/products', productController.insertProduct)
 export default productRouter;
