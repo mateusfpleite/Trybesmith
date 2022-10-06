db0657b46c7a27e9a2b7aaf907b230bf6d3cc0f0
Mateus <mateusfpleite@gmail.com>
2022-10-06 17:45:41 -0300

    Service: creates insertProduct method

--- src/services/Products.service.ts
+++ src/services/Products.service.ts
@@ -1,17 +1,22 @@
-import Products from '../interfaces/products.interface';
+import Product from '../interfaces/products.interfaces';
 import connection from '../models/connection';
 import ProductModel from '../models/Products.model';
 
 class ProductService {
-  public model: ProductModel;
+  private model: ProductModel;
 
   constructor() {
     this.model = new ProductModel(connection);
   }
 
-  public async getAll(): Promise<Products[]> {
+  public async getAll(): Promise<Product[]> {
     const products = await this.model.getAll();
     return products;
+  }
+
+  public async insertProduct(product: Product): Promise<Product> {
+    const newInsertionId = await this.model.insertProduct(product);
+    return { id: newInsertionId, ...product };
   }
 }
 
