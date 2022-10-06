7c44ba648383444045be0eb1b53e0c4891db7381
Mateus <mateusfpleite@gmail.com>
2022-10-06 17:43:49 -0300

    Model: creates insertProduct method

--- src/models/Products.model.ts
+++ src/models/Products.model.ts
@@ -1,17 +1,24 @@
-import { Pool } from 'mysql2/promise';
-import Products from '../interfaces/products.interface';
+import { Pool, ResultSetHeader } from 'mysql2/promise';
+import Product from '../interfaces/products.interfaces';
 
 class ProductModel {
-  public connection: Pool;
+  private connection: Pool;
 
   constructor(connection:Pool) {
     this.connection = connection;
   }
 
-  public async getAll():Promise<Products[]> {
+  public async getAll():Promise<Product[]> {
     const result = await this.connection.execute('SELECT * FROM Trybesmith.Products');
     const [rows] = result;
-    return rows as Products[];
+    return rows as Product[];
+  }
+
+  public async insertProduct(product:Product): Promise<number> {
+    const { name, amount } = product;
+    const query = 'INSERT INTO Trybesmith.Products(name, amount) VALUES (?, ?)';
+    const [{ insertId }] = await this.connection.execute<ResultSetHeader>(query, [name, amount]);
+    return insertId;
   }
 }
 
