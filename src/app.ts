ec2c29871e48ace4e85fbec52f09973d59c62eec
Mateus <mateusfpleite@gmail.com>
2022-10-06 19:51:15 -0300

    creates endpoint POST '/users'

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import productRouter from './Routes/products.routes';
+import userRouter from './Routes/users.routes';
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/', productRouter);
+app.use('/', userRouter);
 
 export default app;
