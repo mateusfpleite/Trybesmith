cc4fba13a6fd3bd8ffeb0f8ea495f407e6448757
Mateus <mateusfpleite@gmail.com>
2022-10-07 16:28:45 -0300

    creates endpoint POST '/login'

--- src/Routes/users.routes.ts
+++ src/Routes/users.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import UserController from '../controllers/User.controller';
+import validateLogin from '../middlewares/loginMiddleware';
 
 const userRouter = Router();
 
@@ -7,4 +8,5 @@ const userController = new UserController();
 
 // productRouter.get('/products', productController.getAll);
 userRouter.post('/users', userController.insertUser);
+userRouter.post('/login', validateLogin, userController.userLogin);
 export default userRouter;
