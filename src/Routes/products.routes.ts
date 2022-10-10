21d4825e2bdc1fe4f17e20f5c2bb2559d15c6c6b
Mateus <mateusfpleite@gmail.com>
2022-10-10 09:50:30 -0300

    add middleware to endpoint

--- src/Routes/products.routes.ts
+++ src/Routes/products.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import ProductController from '../controllers/Product.controller';
+import { productAmountVal, productNameVal } from '../middlewares/insertProductMiddleware';
 
 const productRouter = Router();
 
 const productController = new ProductController();
 
 productRouter.get('/products', productController.getAll);
-productRouter.post('/products', productController.insertProduct);
+productRouter.post('/products', productNameVal, productAmountVal, productController.insertProduct);
 export default productRouter;
